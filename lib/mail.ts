2154b8236f6fb2b9cf92d3d1a69b5025625ea20e
Rio Sinnott <riorio@mailbox.org>
2025-08-17 14:16:53 -0700

    adding logging to pwd reset

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,10 +5,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `http://tfo.creaturetracker.net/reset-password?token=${token}`;
 
-    await resend.emails.send({
+    const { data, error } = await resend.emails.send({
         from: 'noreply@tfo.creaturetracker.net',
         to: email,
         subject: 'Reset Your Password',
         html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`
     });
+
+    if (error) {
+        return console.error({ error });
+    }
+
+    console.log({ data });
 };

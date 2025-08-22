10b08a31e944066cfae9434e208327b8b6f75409
Rio Sinnott <riorio@mailbox.org>
2025-08-22 14:31:03 -0700

    fixing layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,42 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Tektur } from 'next/font/google';
-import "./globals.css"
-import ClientProviders from "./ClientProviders"
+import type React from "react";
+import type { Metadata } from "next";
+import { Tektur } from "next/font/google";
+import "./globals.css";
+import ClientProviders from "./ClientProviders";
 
 export const metadata: Metadata = {
-  title: "TFO.creaturetracker",
-  description: "This is a utility site for the web game The Final Outpost to manage your collection, research goals, and breeding pairs",
-    metadataBase: new URL('https://tfo.creaturetracker.net'),
-}
+    title: "TFO.creaturetracker",
+    description:
+        "This is a utility site for the web game The Final Outpost to manage your collection, research goals, and breeding pairs",
+    metadataBase: new URL("https://tfo.creaturetracker.net"),
+};
 
 const tektur = Tektur({
-  subsets: ['latin'],
-})
+    subsets: ["latin"],
+});
 
 export default async function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-          <head>
-              <link
-                  rel="icon"
-                  href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🧬</text></svg>"
-              ></link>
-              <head>
-                  <link rel="preconnect" href="https://fonts.googleapis.com" />
-                  <link
-                      rel="preconnect"
-                      href="https://fonts.gstatic.com"
-                  />
-                  <link
-                      href="https://fonts.googleapis.com/css2?family=Tektur:wght@400;700&display=swap"
-                      rel="stylesheet"
-                  />
-              </head>
-          </head>
-          <body className="{tektur.className} min-h-screen flex flex-col">
-              <ClientProviders>{children}</ClientProviders>
-          </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🧬</text></svg>"
+                ></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Tektur:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
+            <body className="{tektur.className} min-h-screen flex flex-col">
+                <ClientProviders>{children}</ClientProviders>
+            </body>
+        </html>
+    );
 }

6c208ed0d242643efef2b815dec0ed6ab83a7eb7
Rio Sinnott <riorio@mailbox.org>
2025-09-11 02:48:22 -0700

    small changes to trigger deploy, v1.1.3

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,6 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-    // authToken: process.env.SENTRY_AUTH_TOKEN,
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 1,
     integrations: [

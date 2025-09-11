a555dffdb60e35f271db51a838a665a8465d2f23
Rio Sinnott <rio@savviistack.com>
2025-09-11 14:17:48 -0700

    trying again to fix build error

--- app/api/research-goals/[goalId]/route.ts
+++ app/api/research-goals/[goalId]/route.ts
@@ -158,7 +158,7 @@ export async function PATCH(
     try {
         const body = await req.json();
         const validatedFields = editGoalSchema.safeParse(body);
-        const { fieldErrors } = validatedFields.error?.flatten();
+        const fieldErrors = validatedFields.error?.flatten();
         const errorMessage = Object.values(fieldErrors)
             .flat()
             .join(' ');

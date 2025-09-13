b1e149f3eae971fa2248d2f1dd95bffab175cb67
Rio Sinnott <riorio@mailbox.org>
2025-09-13 14:56:57 -0700

    putting image array back

--- app/share/goals/[goalId]/page.tsx
+++ app/share/goals/[goalId]/page.tsx
@@ -42,7 +42,15 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         openGraph: {
             title,
             description,
-            images: [],
+            images: [
+                {
+                    url: `/share/goals/${goalId}/opengraph-image`,
+                    width: 1200,
+                    height: 630,
+                },
+            ],
+            locale: 'en_US',
+            type: 'website',
         },
         twitter: {
             card: 'summary_large_image',

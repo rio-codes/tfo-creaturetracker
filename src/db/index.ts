3f7b6e098e483e991ba101dadd834b802eff7f62
Rio Sinnott <riorio@mailbox.org>
2025-09-14 18:40:10 -0700

    Revert "fixing ts error, turning on db debug for dark mode issue"
    This reverts commit 35929fea64f3706acedf0912d3e1b1d14787b4c1.

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,4 +10,4 @@ const client = postgres(connectionString, {
     max: 10,
 });
 
-export const db = drizzle(client, { schema, logger: true });
+export const db = drizzle(client, { schema, logger: false });

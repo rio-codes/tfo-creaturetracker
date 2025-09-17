1f01d3ceeca14aead606d4b20931f560b5ce2852
Rio Sinnott <riorio@mailbox.org>
2025-09-16 21:02:12 -0700

    chore(config): 🔧 set up commitlint to prepare for conventional commits
    create commitlint config file with scopes, install required packages

---
+++ commitlint.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [
+            2, // Level: Error
+            'always', // Applicability
+            [
+                // List of allowed scopes
+                // Core Features
+                'collection',
+                'pairs',
+                'goals',
+                'profile',
+                'auth',
+                'settings',
+                // Cross-Cutting Concerns
+                'admin',
+                'ui',
+                'api',
+                'db',
+                'lib',
+                // Project & Tooling
+                'config',
+                'deps',
+                'vercel',
+                'sentry',
+                'docs',
+                'release',
+            ],
+        ],
+    },
+};

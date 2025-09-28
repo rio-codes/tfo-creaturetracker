9bd00269817fc49f3afd7914bc3d2ad642737a5d
Rio Sinnott <riorio@mailbox.org>
2025-09-28 03:04:42 -0700

    fix(config): 🩹 tweak twitter card definition slightly

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
         locale: 'en_US',
     },
     twitter: {
-        card: 'summary_large_image',
+        card: 'summary',
         title: 'TFO.creaturetracker',
         images: 'https://tfo.creaturetracker.net/images/icons/android-chrome-192x192.png',
     },

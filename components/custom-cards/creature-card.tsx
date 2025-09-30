5fa57d98fd4c2f86353a917e1027364f6ab4e6e3
Rio Sinnott <riorio@mailbox.org>
2025-09-29 21:23:31 -0700

    revert(config): 🚨 remove references to react-hooks rule to pass linting

--- components/custom-cards/creature-card.tsx
+++ components/custom-cards/creature-card.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
+
 import { useState, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';

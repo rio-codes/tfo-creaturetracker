2fe36d1081fc22f61d50a2fb2519c334ac7a5c3c
Rio S. (lyricism) <791713+rio-codes@users.noreply.github.com>
2025-10-05 12:30:18 -0700

    fix(profile): ♿️ add tooltip and aria-label to flair icon component for screen readers (#284)
    * fix(profile): ♿️ add tooltip and aria-label to flair icon component for screen readers
    
    * build(deps): ⬆️ update dom-selector and detect-libc
    
    ---------
    
    Co-authored-by: aviator-app[bot] <48659329+aviator-app[bot]@users.noreply.github.com>

--- components/misc-custom-components/flair-icon.tsx
+++ components/misc-custom-components/flair-icon.tsx
@@ -7,17 +7,43 @@ export function FlairIcon({
 }) {
     switch (tier) {
         case 'admin':
-            return <Atom className="inline-block ml-2 text-purple-600" />;
+            return (
+                <Atom className="inline-block ml-2 text-purple-600" aria-label="Admin">
+                    <title>Admin</title>
+                </Atom>
+            );
         case 'beta_tester':
-            return <Microscope className="inline-block ml-2 text-cyan-500" />;
+            return (
+                <Microscope className="inline-block ml-2 text-cyan-500" aria-label="Beta Tester">
+                    <title>Beta Tester</title>
+                </Microscope>
+            );
         case 'researcher':
             return null;
         case 'postdoc':
-            return <Fish className="inline-block ml-2 text-blue-500" />;
+            return (
+                <Fish className="inline-block ml-2 text-blue-500" aria-label="Postdoc (Patron)">
+                    <title>Postdoc (Patron)</title>
+                </Fish>
+            );
         case 'assoc_prof':
-            return <Squirrel className="inline-block ml-2 text-green-500" />;
+            return (
+                <Squirrel
+                    className="inline-block ml-2 text-green-500"
+                    aria-label="Associate Prof. (Patron)"
+                >
+                    <title>Associate Prof. (Patron)</title>
+                </Squirrel>
+            );
         case 'tenured_prof':
-            return <Bird className="inline-block ml-2 text-pink-600" />;
+            return (
+                <Bird
+                    className="inline-block ml-2 text-pink-600"
+                    aria-label="Tenured Prof. (Patron)"
+                >
+                    <title>Tenured Prof. (Patron)</title>
+                </Bird>
+            );
         default:
             return null;
     }

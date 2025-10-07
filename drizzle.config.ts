fa86250a1528d697fd592a2e1f26d6ec90bab734
Rio Sinnott <riorio@mailbox.org>
2025-10-06 20:46:11 -0700

    fix(db): 🐛 remove type check for env var, file should be ignored in eslint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import type { Config } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) {
-    require('dotenv').config({ path: '.env.local' });
-}
+require('dotenv').config({ path: '.env.local' });
 
 export default {
     schema: './src/db/schema.ts',
     out: './src/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL,
     },
 } satisfies Config;

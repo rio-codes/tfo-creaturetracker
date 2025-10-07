14c665505319329d3ae042398bba270479efd046
Rio Sinnott <riorio@mailbox.org>
2025-10-06 21:20:17 -0700

    ci(db): 🐛 only load local env file if in dev environment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'drizzle-kit';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.local' });
+if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: '.env.local' });
+}
 
 export default {
     schema: './src/db/schema.ts',

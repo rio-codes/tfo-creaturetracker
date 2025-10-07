c66119257590ff95693d8f5b88a55400c8fd1068
Rio Sinnott <riorio@mailbox.org>
2025-10-06 20:50:47 -0700

    ci(db): 🐛 updating drizzle config to recommended neon settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'drizzle-kit';
 
-require('dotenv').config({ path: '.env.local' });
-
 export default {
     schema: './src/db/schema.ts',
     out: './src/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL as string,
     },
+    verbose: true,
+    strict: true,
 } satisfies Config;

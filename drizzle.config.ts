2a4a921cdf83d5e8c86b0d56cba09eff831be06f
Rio S. (lyricism) <791713+rio-codes@users.noreply.github.com>
2025-10-11 06:18:45 -0700

    chore(db): 🔧 update pgTable structure to avoid upcoming deprecation, fixes #269 (#310)
    fixes #269

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,13 +5,17 @@ if (process.env.NODE_ENV === 'development') {
     dotenv.config({ path: '.env.local' });
 }
 
+const database_url = process.env.DATABASE_URL;
+if (!database_url) {
+    throw new Error('DATABASE_URL is not defined');
+}
+
 export default {
     schema: './src/db/schema.ts',
     out: './src/db/migrations',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL as string,
+        url: database_url as string,
     },
     verbose: true,
-    strict: true,
 } satisfies Config;

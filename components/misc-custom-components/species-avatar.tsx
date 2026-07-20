7801dc5cd90e4209ebee47536dadefb955333292
Rio Sinnott <riorio@mailbox.org>
2026-07-20 10:05:53 -0700

    fix(genetics): support Kora Voko species alias and code SPtOw

--- components/misc-custom-components/species-avatar.tsx
+++ components/misc-custom-components/species-avatar.tsx
@@ -197,6 +197,7 @@ export function SpeciesAvatar({ species, className }: { species: string; classNa
             return (
                 <img src="/images/capsules/koleratako_capsule.png" className={imgClassName}></img>
             );
+        case 'Kora Voko':
         case 'Koro Voko':
             return (
                 <img src="/images/capsules/Koro_voko_capsule.png" className={imgClassName}></img>
